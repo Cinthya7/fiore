c96b137099a2a4a1c65269381d732808c296b583
idelta777 <manuel777@outlook.com>
2017-05-28 18:18:19 -0500

    Cambia letra ñ

--- src/vistas/index.php
+++ src/vistas/index.php
@@ -12,7 +12,7 @@
 				<td><input type="text" name="usuario"/></td>
 			</tr>
 			<tr>
-				<td>ContraseÃ±a</td>
+				<td>Contraseña</td>
 				<td><input type="password" name="pass"/></td>
 			</tr>
 			
